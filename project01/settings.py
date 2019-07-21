73c49e810e2e9ea5e1b6f6faf0dd0f3fbcd600b6
pm-lab-polito <50363223+pm-lab-polito@users.noreply.github.com>
2019-07-21 10:00:25 +0200

    models

--- project01/settings.py
+++ project01/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['18.185.114.239']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
